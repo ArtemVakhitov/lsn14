ee2a4e1e1a556911aa8acd777f723ad72d9f090c
Artem Vakhitov <artem.vakhitov@gmail.com>
2024-10-02 11:48:53 +0300

    Initial commit, creates 1 fixed instance

---
+++ config.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone = "ru-central1-b"
+}
+
+resource "yandex_compute_instance" "first" {
+
+  name = "first"
+
+  zone = "ru-central1-b"
+  platform_id = "standard-v1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8tvc3529h2cpjvpkr5"
+    }
+  }
+
+  network_interface {
+    subnet_id = "e2lgv5mqm56n8fjkt37q"
+    nat = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
+  }
+
+}
